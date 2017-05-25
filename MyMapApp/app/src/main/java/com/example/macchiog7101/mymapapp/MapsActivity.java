e1d51f3d23fe271b8d7f12a584706223340e6993
Frogman1117 <gregory.macchio@gmail.com>
2017-05-25 11:03:36 -0700

    5/25/17, Steps 1-3 Done

--- MyMapApp/app/src/main/java/com/example/macchiog7101/mymapapp/MapsActivity.java
+++ MyMapApp/app/src/main/java/com/example/macchiog7101/mymapapp/MapsActivity.java
@@ -39,8 +39,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng birth = new LatLng(38, -77);
+        mMap.addMarker(new MarkerOptions().position(birth).title("Born here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(birth));
+
+        LatLng currentPostion = new LatLng(38, -77);
+        //http://blog.teamtreehouse.com/beginners-guide-location-android
     }
+
 }
